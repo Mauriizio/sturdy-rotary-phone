3c1548431fa181e7482c5e3f10047c9c48696af4
Maurizio <caballeromaurizio@gmail.com>
2025-06-26 22:24:36 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-pink-600">¡Dulces Secretos 🍰!</h1>
+      <p className="mt-4 text-gray-700">Bienvenidos a la página oficial de repostería</p>
+    </div>
+  );
+}
+
+export default App;
